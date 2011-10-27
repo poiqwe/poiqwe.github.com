756247671325c5c98f7099fae370d2b26261ccbc
unknown <Resty@.(none)>
2011-10-27 16:57:05 -0400

    zipcode fix

--- PostageCalculator.js
+++ PostageCalculator.js
@@ -3,6 +3,7 @@
 
 function validate(form)
 {	
+	document.getElementById("lblErrMsg").innerHTML = "";
 	// Be sure to clear out old error messages before you do your validation!
 	// An example of how to set the lblPostage to empty
     document.getElementById("lblPostage").innerHTML = "";
@@ -10,16 +11,16 @@ function validate(form)
 	// To access the value of a form input, we do form.inputname.value
 	// For example, to check if the first name is empty we do:
 	
-	if (form.firstname.value == "")
+	if (form.txtFirstName.value == "")
 		alert("You left the first name empty!"); // Alert the user that they left the first name empty
 	
 	// You'll also need to check if their zip code is valid. You should use regular expresions to do this.
 	// Be sure to format your regex properly so that it only allows valid, 5-digit zip codes
 	// Below is an example of how to use regular expressions
-	var zipRegex="/\d{5}/";
+	var zipRegex=/\d{5}/;
 	
-	if (form.srcZipcode.value.search(zipRegex) == -1)
-		document.getElementById("errSrcZipcode").innerHTML = "Invalid zip code";
+	if (form.txtSrcZipCode.value.search(zipRegex) == -1 || form.txtSrcZipCode.value.length != 5)
+		document.getElementById("lblErrMsg").innerHTML += "Invalid zip code<br/>";
 	
 	// Also, make sure their weight isn't zero or negative!
 	// You'll need to convert the weight from your form into a number
@@ -28,6 +29,6 @@ function validate(form)
 	
 	// If you want to write your result or any errors to the page
 	// You'll need to use the getElementById method:
-	document.getElementById("postage").innerHTML = 500; // Set postage to 500
+	document.getElementById("lblPostage").innerHTML = 500; // Set postage to 500
 	
 }
