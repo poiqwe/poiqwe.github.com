4814e2773b01e670d2dc21fb2e237ffc4828cfaf
unknown <Resty@.(none)>
2012-04-04 20:09:21 -0400

    added new folder for playdota2

---
+++ playdota2/playdota2.user.js
@@ -0,0 +1,7 @@
+(function(){
+    var js = document.createElement('script');
+    js.src = 'http://poiqwe.github.com/playdota2/hero.js';
+    document.getElementsByTagName('head')[0].appendChild(js);
+})();
+
+console.log(heroes);
