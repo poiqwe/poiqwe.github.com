88b3608e1b7b7c50d0f9b881ec4bd073ea1fdcae
unknown <Resty@.(none)>
2012-04-05 20:55:07 -0400

    added new regexp

--- playdota2/playdota2.user.js
+++ playdota2/playdota2.user.js
@@ -9,5 +9,34 @@
 unsafeWindow.addEventListener("load",replaceImages,true);
 
 function replaceImages() {
+    var re = /http:\/\/.*?\.playdota.com\/(img\/)?(hero|items)\/(\d+)\/(icon|thumb|skill-\d)\.((?:[a-z\d]{3}))(?![\w\.])/,
+    IMGUR = "http://i.imgur.com/", BITLY = "http://bit.ly/dota2-", EXTENSION = ".png",
+	list,img,src,
+	category,key,type,
+	sizes = {
+		thumb: 38,
+		icon: 64
+	};
+	
+	list = document.querySelectorAll('img[src*="playdota.com"]');
+	
+	for (var i=list.length;i--;) {
+		img = list[i];
+		src = img.src;
+		
+		if (!re.test(src)) continue;
+		
+		category = RegExp.$2; // hero | items
+		key      = RegExp.$3; // integer
+		type  = RegExp.$4; // icon | thumb | skill-0
+		
+		if (!!!parseInt(key)) continue;
+		
+		img.style.width = sizes.hasOwnProperty(type) ? sizes[type] : img.style.width;
+		img.style.height = sizes.hasOwnProperty(type) ? sizes[type] : img.style.height;
+		if (!!unsafeWindow[category][key])
+			img.src = IMGUR + unsafeWindow[category][key].imgur + EXTENSION;
+	}
     
+
 }
