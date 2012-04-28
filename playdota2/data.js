05ae0e9bae76692f16a2bc004f5160f8348aa255
unknown <Resty@Resty-PC.(none)>
2012-04-27 23:08:42 -0400

    added item 80

--- playdota2/data.js
+++ playdota2/data.js
@@ -244,5 +244,6 @@ var data = {
 data.items[7] = data.items[769];
 data.items[8] = data.items[770];
 data.items[54] = data.items[773];
+data.items[80] = data.items[775];
 data.items[112] = data.items[772];
 data.items[113] = data.items[771];
